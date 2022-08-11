070da26e8ebc379b0718662f5686aeab68e7c299
Utkarsh Saxena <utkars.saxena@icloud.com>
2022-08-11 18:54:21 -0400

    create columns with random number of items

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,14 +1,14 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { Column } from "./components/Column/Column";
 
 import "./Board.css";
 
-const NUMBER_OF_COLUMNS = 3;
+const NUMBER_OF_COLUMNS = 5;
 
 function Board() {
   const columnMarkup = useMemo(() => {
     return new Array(NUMBER_OF_COLUMNS).fill(true).map((_, index) => {
-      return <Column />;
+      return <Column items={new Array(getRandomInt(5, 10)).fill(true)} />;
     });
   }, []);
 
@@ -22,6 +22,12 @@ function Board() {
       {columnMarkup}
     </div>
   );
+}
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export default Board;
--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,5 +1,22 @@
+import { useMemo } from "react";
 import "./Column.css";
 
-export function Column() {
-  return <div className="Column">Hello</div>;
+interface Props {
+  items: any[];
+}
+
+export function Column(props: Props) {
+  const { items } = props;
+
+  const listItemsMarkup = useMemo(() => {
+    return new Array(items.length).fill(true).map((_, index) => {
+      return <li className="Column-ListItem">{index}</li>;
+    });
+  }, [items]);
+
+  return (
+    <div className="Column">
+      <ul className="Column-ListContiner">{listItemsMarkup}</ul>
+    </div>
+  );
 }
